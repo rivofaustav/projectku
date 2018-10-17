9f134e58486df2a6b8edbc5b3bf5c6a8737d0bb3
Rivo <Rivo@172.16.182.250>
2018-10-17 22:50:05 +0700

    tes

--- UTS/midterm-pbo-master/src/id/ac/polinema/midterm/DiscountItem.java
+++ UTS/midterm-pbo-master/src/id/ac/polinema/midterm/DiscountItem.java
@@ -16,4 +16,16 @@ public class DiscountItem extends Item{
         super(name, price, amount);
         this.discount = discount;
     }
+    
+    public float getDiscount(){
+        return (amount*price)*discount;
+    }
+    
+    public float getTotalPrice(){
+        return amount*price-getDiscount();
+    }
+    
+    public String toString(){
+        return "|"+name+"\t\t"+"|"+price+"\t"+"|"+amount+"\t\t"+"|"+getTotalPrice()+"\t|"+"\n|-Disc"+"\t\t"+"|"+discount*100+"%\t\t|"
+    }
 }
--- UTS/midterm-pbo-master/src/id/ac/polinema/midterm/PromoItem.java
+++ UTS/midterm-pbo-master/src/id/ac/polinema/midterm/PromoItem.java
@@ -17,4 +17,12 @@ public class PromoItem extends Item{
         this.promo = promo;
         
     }
+    
+    public float getTotalPrice(){
+        return (amount*price)-(promo*amount);
+    }
+    
+    public String toString(){
+        return "|"+name+"\t\t"+"|"+price+"\t|"+amount+"\t\t"+"|"+getTotalPrice()+"\t|"+"\n|-promo"+"\t\t"+"|"+promo+"\t\t|\t\t|\t\t";
+    }
 }
