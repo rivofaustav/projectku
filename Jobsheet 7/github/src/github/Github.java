5cf53453210a3e55ee9bb6664ae62d9edc4d0606
Rivo <Rivo@172.16.182.250>
2018-10-17 07:41:28 +0700

    setup project

--- Jobsheet 7/github/src/github/Github.java
+++ Jobsheet 7/github/src/github/Github.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package github;
 
 /**
@@ -11,11 +7,7 @@ package github;
  */
 public class Github {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(".");
     }
-    
 }
